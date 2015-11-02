7d33231f59371e09f7c88eb6c074e111a3d2bf4a
djres88 <davidj.resnick@gmail.com>
2015-11-02 14:07:54 -0500

    pushing codequizzes site mockup (v1)

---
+++ codequizzes-mockup/cardEffects.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("#cardChapter1").flip({
+    trigger: "hover",
+    reverse: "true"
+  })
+  $("#cardChapter2").flip({
+    trigger: "hover",
+    reverse: "true"
+  })
+  $("#cardChapter3").flip({
+    trigger: "hover",
+    reverse: "true"
+  })
+})

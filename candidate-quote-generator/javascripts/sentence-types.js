fdae8fe3a36b2057ea901eee430f9858a1aaa9d4
djres88 <davidj.resnick@gmail.com>
2015-12-30 12:11:43 -0600

    Adding candidate quote generator project files (in progress)

---
+++ candidate-quote-generator/javascripts/sentence-types.js
@@ -0,0 +1,15 @@
+var noun;
+var plural-noun;
+var verb;
+var adverb;
+var adjective;
+
+
+var sentenceTypes = [
+  noun + " " + verb + " " + noun,
+
+}
+
+function generateSentence() {
+  return getRandom(sentenceType);
+}
---
+++ candidate-quote-generator/quotegenerator.js
@@ -0,0 +1,40 @@
+words = [
+  {
+    word: "the",
+    part_of_speech: "art"
+  },
+  {
+    word: "china",
+    part_of_speech: "noun"
+  },
+  {
+    word: "immmigrant",
+    part_of_speech: "noun"
+  }
+]
+
+nouns = []
+for (var i = 0; i < words.length; i++) {
+  if (words[i].part_of_speech === "noun") {
+    nouns.push(words[i]);
+  }
+}
+
+console.log(nouns)
+
+var pullCandidates = {
+
+}
+
+var wordGetter = {
+  words:
+  nouns:
+  randomNoun:
+  randomAdj:
+}
+
+var sentenceConstructor = {
+
+}
+
+// predicted concession speeches. Mock debates

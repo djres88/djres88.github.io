b7c6b09aac5a9aa5060b6ea4ac70ba3fcb5d89f7
djres88 <davidj.resnick@gmail.com>
2016-01-04 12:05:49 -0600

    Add random person (subject)

--- candidate-quote-generator/javascripts/make-sentence.js
+++ candidate-quote-generator/javascripts/make-sentence.js
@@ -1,10 +1,11 @@
 // testing
 var adjective = ["typical", "radical", "Islamist", "unbelievable"];
-var subject = ["I", "you", "we", "Obama", "ISIS", "debt"];
+var subject = ["Obama", "ISIS", "debt"];
 var adverb = ["slowly", "quickly", "repeatedly"];
 var verb = ["build", "blow up", "need", "fire", "glow"];
-var object = ["wall", "debt ceiling", "the White House"];
+var object = ["wall", "debt ceiling", "White House"];
 
+//The following array includes several potential sentence types. The SENTENCE GENERATOR -- see function below -- randomly selects one of these types before filling in the sentence with words from a random candidate's Twitter feed. You can add new sentence types to the end of the array (but don't forget the commas).
 var sentenceType = [
   "subject adverb verb adjective object",
   "subject verb object and the subject verb object",
@@ -17,11 +18,28 @@ var sentenceType = [
   "subject must adverb verb object"
 ];
 
+var person = ["first", "second", "third", "first-plural", "third-plural"];
+
+function conjugate(subject) {
+  if (person === "first") {
+    myVerb = myVerb[0];
+  } if (person === "second") {
+    myVerb = myVerb[1];
+  } if (person === "third") {
+    myVerb = myVerb[2];
+  } if (person === "first-plural") {
+    myVerb = myVerb[3];
+  } if (person === "third-plural") {
+    myVerb = myVerb[4];
+  }
+};
+
 function generateSentence() {
+  var myPerson = getRandom(person);
   var mySentence = getRandom(sentenceType).split(" ");
   for (var i = 0; i < mySentence.length; i++) {
     if (mySentence[i] === "subject") {
-      mySentence[i] = getRandom(subject);
+      mySentence[i] = getRandom(subject)[myPerson];
     } if (mySentence[i] === "adjective") {
       mySentence[i] = getRandom(adjective);
     } if (mySentence[i] === "adverb") {
@@ -36,17 +54,3 @@ function generateSentence() {
   mySentence = mySentence.charAt(0).toUpperCase() + mySentence.slice(1) + ".";
   return mySentence;
 }
-
-function conjugate(subject) {
-  if (mySubject === "first") {
-    myVerb = myVerb[0];
-  } if (mySubject === "second") {
-    myVerb = myVerb[1];
-  } if (mySubject === "third") {
-    myVerb = myVerb[2];
-  } if (mySubject === "first-plural") {
-    myVerb = myVerb[3];
-  } if (mySubject === "third-plural") {
-    myVerb = myVerb[4];
-  }
-};

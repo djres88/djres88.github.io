64dee761a92e27b8f79910d0c71cc613b775ff56
djres88 <davidj.resnick@gmail.com>
2016-01-06 15:42:37 -0600

    Adding twitter scraping js files for hillary, bernie. Need to run, put words in excel db.

--- candidate-quote-generator/javascripts/twitter-scraping/donald-trump.js
+++ candidate-quote-generator/javascripts/twitter-scraping/donald-trump.js
@@ -11,8 +11,18 @@ var array = [];
 streamOfTweets.on('readable', function() {
   var tweet = streamOfTweets.read().text;
   if (tweet.indexOf("@") === -1) {
-    array.push(tweet);
+    var words = tweet.split(" ");
+    for (var i = 0; i < words.length; i++) {
+      if (!/[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(words[i])) {
+        array.push(words[i].toLowerCase());
+      }
+    }
   }
 });
+
+setTimeout(function() {
+  array.split
+  console.log(array.sort())
+}, 15000)
 
 exports.array = array;
---
+++ candidate-quote-generator/javascripts/twitter-scraping/hillary-clinton.js
@@ -0,0 +1,28 @@
+var TwitterPosts, streamOfTweets;
+TwitterPosts = require('twitter-screen-scrape');
+
+streamOfTweets = new TwitterPosts({
+  username: //insert hillary clinton handle here
+  retweets: false
+});
+
+var array = [];
+
+streamOfTweets.on('readable', function() {
+  var tweet = streamOfTweets.read().text;
+  if (tweet.indexOf("@") === -1) {
+    var words = tweet.split(" ");
+    for (var i = 0; i < words.length; i++) {
+      if (!/[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(words[i])) {
+        array.push(words[i].toLowerCase());
+      }
+    }
+  }
+});
+
+setTimeout(function() {
+  array.split
+  console.log(array.sort())
+}, 15000)
+
+exports.array = array;

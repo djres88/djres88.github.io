11a94f0a51d757f481b7f526d667bfd37e2f3aa5
djres88 <davidj.resnick@gmail.com>
2016-01-06 18:09:26 -0600

    Adding near-complete files for Trump

---
+++ candidate-quote-generator/javascripts/sentence-constructor.js
@@ -0,0 +1,46 @@
+// //For Node
+// //Load random selector function
+// var r = require('./randomize.js');
+// //Load sentence types.
+// var s = require('./sentence-types.js');
+// //Load words db.
+// var w = require('../words-db/testing.js');
+
+function generateSentence() {
+  var mySentence = getRandom(sentenceType).split(" ");
+  // Replace nouns first. To maintain English's "article/possessive- (adjective)- noun" syntax, you need keep the "adjective" strings intact through the initial loop.
+  for (var i = 0; i < mySentence.length; i++) {
+    if (mySentence[i] === "noun" || mySentence[i] === "noun-subject") {
+      var myNoun = getRandom(noun);
+      //If the current noun is the subject, grab the subject's person and store for conjugating the verb:
+      if (mySentence[i] == "noun-subject") {
+        var conjugation = myNoun["person"];
+      }
+      mySentence[i] = myNoun["word"];
+      //Add an article or possessive, if myNoun requires one.
+      if (myNoun["needsArticle"] === "yes") {
+        //Add the article to accomodate an optional/random adjective in the middle. (English is silly that way: "the car" with an adjective becomes "the green car." That's versus, say, Spanish, where the adjective is just tagged onto the end: "el coche" becomes "el coche verde.")
+        if (mySentence[i-1] === "adjective") {
+          mySentence.splice(i-1, 0, getRandom(articlesEtc));
+        } else {
+          mySentence.splice(i, 0, getRandom(articlesEtc));
+        }
+      }
+    }
+  }
+  // Cycle through the sentence and replace other parts of speech (adjectives, adverbs, and verbs) with a random word from the candidate's vocabulary:
+  for (var i = 0; i < mySentence.length; i++) {
+    if (mySentence[i] === "adjective") {
+      mySentence[i] = getRandom(adjective);
+    }
+    if (mySentence[i] === "adverb") {
+      mySentence[i] = getRandom(adverb);
+    }
+    if (mySentence[i] === "verb") {
+      mySentence[i] = getRandom(verb)[conjugation];
+    }
+  };
+  mySentence = mySentence.join(" ");
+  mySentence = mySentence.charAt(0).toUpperCase() + mySentence.slice(1) + ".";
+  return mySentence;
+}
---
+++ candidate-quote-generator/javascripts/sentence-types.js
@@ -0,0 +1,9 @@
+//The following array includes several potential sentence types. The SENTENCE GENERATOR -- see function below -- randomly selects one of these types before filling in the sentence with words from a random candidate's Twitter feed. You can add new sentence types to the end of the array (but don't forget the commas).
+var sentenceType = [
+  "noun-subject adverb verb adjective noun",
+  "I promise to verb noun adverb",
+  "noun-subject verb adjective noun"
+];
+
+// //node
+// exports.sentenceType = sentenceType;

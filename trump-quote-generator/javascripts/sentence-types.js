19864d14263b244a06b3ccf5ad8f6909d9b45152
djres88 <davidj.resnick@gmail.com>
2016-01-08 17:32:47 -0800

    fixed bug

--- trump-quote-generator/javascripts/sentence-types.js
+++ trump-quote-generator/javascripts/sentence-types.js
@@ -6,7 +6,7 @@ var sentenceType = [
   "noun-subject adverb verb noun as well as noun",
   "I say noun-subject verb noun",
   "noun-subject verb adjective noun",
-  "noun-subject verb, only to verb noun",
+  "noun-subject verb , only to verb noun",
   "Which noun-subject verb",
   "let's make noun adjective again",
   "noun-subject , adjective noun , verb noun",

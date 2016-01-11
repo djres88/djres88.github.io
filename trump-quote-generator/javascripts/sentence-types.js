af422201a1db1390ddfdbbe6c0297679de5509a4
djres88 <davidj.resnick@gmail.com>
2016-01-10 23:08:43 -0800

    Removed adverbs. Need to change adverb array to objects, add property that indicates position in sentence

--- trump-quote-generator/javascripts/sentence-types.js
+++ trump-quote-generator/javascripts/sentence-types.js
@@ -1,9 +1,7 @@
 //The following array includes several potential sentence types. The SENTENCE GENERATOR -- see function below -- randomly selects one of these types before filling in the sentence with words from a random candidate's Twitter feed. You can add new sentence types to the end of the array (but don't forget the commas). NOTE: for formatting reasons, commas should be preceded by a space.
 
 var sentenceType = [
-  "noun-subject adverb verb adjective noun",
   "noun-subject verb noun",
-  "noun-subject adverb verb noun as well as noun",
   "I say noun-subject verb noun",
   "noun-subject verb adjective noun",
   "noun-subject verb noun , but not adjective noun",

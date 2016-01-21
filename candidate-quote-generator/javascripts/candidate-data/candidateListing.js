851fa6b96620f9302d7ff47d58cbb50e6384a847
djres88 <davidj.resnick@gmail.com>
2016-01-20 18:09:57 -0800

    Adding files for each candidate to use as lookups when adding a speaker property to each word.

---
+++ candidate-quote-generator/javascripts/candidate-data/candidateListing.js
@@ -0,0 +1,6 @@
+var candidates = [
+  {speaker: "hillary-clinton", words: clintonWords},
+  {speaker: "donald-trump", words: trumpWords},
+  {speaker: "ted-cruz", words: cruzWords},
+  {speaker: "bernie-sanders", words: sandersWords}
+];

99dc974787d6bc9fa53d96c8c173da8f2e86a082
djres88 <davidj.resnick@gmail.com>
2016-01-21 13:17:28 -0800

    Reorganized helper functions and word objects.

--- candidate-quote-generator/javascripts/candidates/wordlist_donald-trump.js
+++
@@ -1,1 +0,0 @@
-var trumpWords = ["go", "believe"];
--- candidate-quote-generator/javascripts/formatted-word-objects/addSpeakerProperty.js
+++ candidate-quote-generator/javascripts/formatted-word-objects/addSpeakerProperty.js
@@ -1,7 +1,7 @@
 //Instead of having a noun/verb/adjective/etc object for EACH candidate, I'm adding a "speaker" property to each word-object. This method makes for an easy cut/paste of words into the "wordlist" files.
 var addSpeaker = function(candidateName, candidateWords) {
   return function(allWords) {
-    allWords.map(function(word) {
+    allWords.forEach(function(word) {
       //The return function checks every word in the object for a matching word in the candidate's db. If it finds one, it adds the candidate's name to the "speaker" property in the word object.
       candidateWords.forEach(function(candidateWord) {
         if (candidateWord === word.lookupValue) {
@@ -9,12 +9,11 @@ var addSpeaker = function(candidateName, candidateWords) {
         };
         word.speaker = unique(word.speaker);
       });
-      return word;
     });
   };
 };
 
-var partsOfSpeech = [verbs];
+var partsOfSpeech = [adjectives, adverbs, articles, nouns, prepositions, verbs];
 
 var addAllSpeakers = function(myCandidates, myNames, myWords) {
   myCandidates.forEach(function(candidate) {
@@ -28,11 +27,9 @@ var addAllSpeakers = function(myCandidates, myNames, myWords) {
 
 var objectsWithSpeakers = addAllSpeakers(candidates, "speaker", "words");
 
-// var adjectives = objectsWithSpeakers[0];
-// var adverbs = objectsWithSpeakers[1];
-// var articles = objectsWithSpeakers[2];
-// var nouns = objectsWithSpeakers[3];
-// var prepositions = objectsWithSpeakers[4];
-var verbs = objectsWithSpeakers[0];
-
-// adjectives, adverbs, articles, nouns, prepositions,
+var adjective = objectsWithSpeakers[0];
+var adverb = objectsWithSpeakers[1];
+var article = objectsWithSpeakers[2];
+var noun = objectsWithSpeakers[3];
+var preposition = objectsWithSpeakers[4];
+var verb = objectsWithSpeakers[5];

3dfb991e33ef53494e4901590cb5be5584d98374
djres88 <davidj.resnick@gmail.com>
2016-01-25 12:26:08 -0800

    Switching format of candidate words final

---
+++ candidate-quote-generator/javascripts/candidate-words-final/hillary-clinton.js
@@ -0,0 +1,10 @@
+//All six parts of speech stored as variables w/ the word-objects inside.
+//FORMAT:
+// var donald = {
+//   adjectives: {
+//     //all adjectives go here
+//   },
+//   nouns: {
+//     //all nouns go here
+//   }
+// }

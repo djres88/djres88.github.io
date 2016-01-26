733bfbdb4bc88494f93c1762bc610cac4b1276c9
djres88 <davidj.resnick@gmail.com>
2016-01-25 19:55:06 -0800

    Working version. Scrapes each candidate's twitter, removes duplicates and quotes/retweets (we're looking only for candidate's own words

--- candidate-quote-generator/javascripts/make-word-objects/twitter-scraper.js
+++ candidate-quote-generator/javascripts/make-word-objects/twitter-scraper.js
@@ -18,19 +18,18 @@ function getTweets(handle) {
 
   //Give the scraper some time -- at least 60 seconds (to be safe) -- to pull all available tweets from each handle. Generally the scaper takes ~800 tweets, as that's how far back Twitter's UI goes.
   setTimeout(function() {
-    var directQuotesOnly = tweets.filter(function(tweet) { return tweet[0] !== "\""; }); //Some tweets are quotes from other people. Remove these -- only want the candidate's words.
-    var string = directQuotesOnly.join(" ").replace(/[\".&?:,\-\—!\(\)]/g, "").replace(/\n/g, " ").toLowerCase(); //Join all words into a single string and format.
-    wordsArray = string.split(" ").sort().filter(function(word) { //Split the words back into an array so that each word is its own element, then sort and filter the array.
+    var directQuotesOnly = tweets.filter(function(tweet) { return tweet[0] !== "\""; }); //Some tweets are quotes from other people. Filtering by "retweets: false" takes care of most of these, but Trump likes to cut/paste quotes rather than retweeting. But he DOES include a leading quote (\") when retweeting, so I filter them out that way.
+    var wordsArray = directQuotesOnly.join("").split(" ").sort().filter(function(word) { //Split the words back into an array so that each word is its own element, then sort and filter the array.
       return !word.match(/(http|pictwitter|#|@)/); // Filter words that are... actual words (i.e. not websites, hashtags, etc.)
     });
 
     words = unique(wordsArray); // Get only unique words (no need to store duplicates in the array)
 
-    console.log("**********HERE ARE THE WORDS FOR", handle, "*********" words);
-  }, 60000);
+    console.log("**********HERE ARE THE WORDS FOR", handle, "*********", wordsArray);
+  }, 10000);
 }
 
 getTweets("realDonaldTrump");
-getTweets("HillaryClinton");
-getTweets("BernieSanders");
-getTweets("TedCruz");
+// getTweets("HillaryClinton");
+// getTweets("BernieSanders");
+// getTweets("TedCruz");

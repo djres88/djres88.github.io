ae7c3c5f2029d22f1316c2d5737bc03cd13ba43c
djres88 <davidj.resnick@gmail.com>
2016-02-02 00:18:15 -0800

    Removing archive files

--- candidate-quote-generator/archive/constructors.js
+++
@@ -1,8 +0,0 @@
-//verb constructor
-function addVerb(first,second,third,firstPlural,thirdPlural) {
-  this.first = first;
-  this.second = second;
-  this.third = third;
-  this.firstPlural = firstPlural;
-  this.thirdPlural = thirdPlural;
-}

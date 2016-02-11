00e9066c44485c4866b9cc4228d251fd90bc6684
djres88 <davidj.resnick@gmail.com>
2016-02-11 15:29:41 -0800

    Testing sentence generator. Works at getting words in the right order, now just need to (a) conjugate verb based on subject and (b) pull the strings from each object.

--- candidate-quote-generator/test_gen.js
+++ candidate-quote-generator/test_gen.js
@@ -1,136 +1,47 @@
-function randomObjectKey(input) {
-  return Math.floor(Math.random()*Object.keys(input).length);
-}
-
-function randomArrayElement(input) {
-  return Math.floor(Math.random()*input.length);
-}
-
-var getRandom = function(input) { // Sometimes I'll just want a random object, without going on until I get a string.
-  var index = 0, result;
-  if (Array.isArray(input)) {
-    index = randomArrayElement(input);
-    result = input[index];
-  } else {
-    index = randomObjectKey(input);
-    var keyName = Object.keys(input)[index];
-    result = input[keyName];
-  }
-  return result;
-}
-
-var sentenceTest = [ { word: 'somebody',
-    speech: 'pronoun-singular',
-    person: 'third',
-    articles: [ '' ] },
-  { word: 'receive',
-    speech: 'transitive-verb',
-    "present":
-     { "first": 'receive',
-       "second": 'receive',
-       "third": 'receives',
-       "plural": 'receive' },
-    "future":
-     { "first": 'will receive',
-       "second": 'will receive',
-       "third": 'will receive',
-       "plural": 'will receive' } },
-  { word: 'word',
-    speech: 'singular-noun',
-    person: 'third',
-    articles: [ 'the', 'a', [Object], [Object] ] } ];
-
-
-//sentenceStructure includes several potential sentence types. Each sentence type is followed by the position of the subject
-var sentenceStructure = [
-  "subject transitive-verb noun",
-  "subject transitive-verb adjective noun"
-];
-
-var subjectPosition; //Gets the index of the subject in the sentence. Used for subject/verb agreement later.
-var tense = ["present", "future"]; // I'll add the past tense once I find a reliable table of irregular verb patterns.
-
-function isAdjective(word) {
-  return Boolean(word["speech"] === "adjective")
-}
-
-function isVerb(word) {
-  return Boolean(word["speech"].match(/-verb/))
-}
-
-function isNoun(entry) {
-  return Boolean(entry["speech"].match(/noun/))
-}
-
-function addArticle(sentence, index) {
-  var article_for_this_word = sentence[index]["articles"];
-  if (index === 0) {
-    sentence.splice(index, 0, getRandom(article_for_this_word));
-  } else if (isAdjective(sentence[index-1])) {
-    sentence.splice(index-1, 0, getRandom(article_for_this_word));
-  } else {
-    sentence.splice(index, 0, getRandom(article_for_this_word));
-  }
-}
-
-function randomMatchingWord(partOfSpeech, candidateWords) {
-  var wordsOfThisType = candidateWords.filter(function(entry) {
-    return entry["speech"] === partOfSpeech;
+//Given a part of speech and a candidate, this function looks in the candidate's vocabulary for words that match the part of speech and returns a random one.
+function randomWordOfGivenType(partOfSpeech, candidate) {
+  var wordOfThisType = candidate.filter(function(entry) {
+    return entry["speech"].match(partOfSpeech);
   });
-
-  if (wordsOfThisType[0]) { // Test whether the array has any elements.
-    return getRandom(wordsOfThisType);
-  } else if (partOfSpeech === "subject" || partOfSpeech === "noun") { //Replace the subject and all unspecified nouns with a random noun.
-    var possibleNouns = ["singular-noun", "plural-noun", "mass-noun", "pronoun-singular", "pronoun-plural", "proper-noun"];
-    return randomMatchingWord(getRandom(possibleNouns), candidateWords); //rerun the function recursively, this time with a part of speech that will trigger the first logic.
-  } else {
-    return "..."; // A catchall in case I mess up one of the sentence structures. Instead of looking like... error... looks like... pause.
-  }
+  return getRandom(wordOfThisType);
 }
 
-//The following function constructs a series of word objects according to a randomly selected "sentence type".
-function sentenceOfObjects(string, candidate) {
-  var arr = string.split(" "); // splits the string into an array so that you can look up each part-of-speech type.
-  subjectPosition = arr.indexOf("subject")
-  var objectsInSentence = arr.map(function(word) {
-    return randomMatchingWord(word, candidate);
-  });
-  return objectsInSentence;
+function tagSubject(word, candidateWords) {
+  var subject = randomWordOfGivenType("noun", candidateWords);
+  subject.isSubject = "yes"
+  return subject;
 }
 
-// function conjugate(sentence) {
-//   var person = sentence[subjectPosition].
-// }
-//Now what's needed is to "conjugate" the verb according to the noun-subject's part of speech, and to pull the strings (words) from each object.
-
-
-//Run the functions
-console.log(sentenceOfObjects(getRandom(sentenceStructure), sentenceTest))
-function generateFinalSentence() {
-  var sentence = sentenceOfObjects(getRandom(sentenceStructure), sentenceTest);
-  for (var i = 0; i < sentence.length; i++) {
-
-    var word = sentence[i];
-    var updated_word;
-
-    if (word === "") {
-      break;
-    } else if (isVerb(word)) {
-      var myTense = getRandom(tense);
-      var person = sentence[subjectPosition]["person"];
-      updated_word = word[myTense][person]; // in a random tense, find a verb conjugation that matches the person of the subject
-    } else if (isNoun(word)) {
-      addArticle(sentence, i);
-      i++; // need to increment one extra since the sentence is now one word longer with the added article.
-    } else {
-      updated_word = word["word"];
+function randomSentenceOfObjects(grammaticalString, candidateWords) {
+  var grammaticalArray = grammaticalString.split(" "); // splits the string into an array so that you can look up each part-of-speech type.
+  var sentence = grammaticalArray.map(function(word) {
+    if (word === "subject") {
+      return tagSubject(word, candidateWords);
+    } else {// Add a "tag" to the subject. Used to conjugate later.
+    return randomWordOfGivenType(word, candidateWords);
     }
-  }
-  return sentence;
+  });
+
+  return sentence
 }
 
-generateFinalSentence()
+function formatWordOrder(sentence) {
+  var wordsInCorrectOrder = [];
+  var spliceAtAdjective = wordsInCorrectOrder.length-1;
 
-  //   sentence[i] = getWord; // replace this word with the formatted word from the object
-  // }
-  // return sentence.join(" ");
+  sentence.forEach(function(word, idx) {
+    if (isNoun(word)) {
+      var articlesForThisWord = word["articles"]
+      if (idx === 0 || !isAdjective(sentence[idx-1])) { // As long as there's no adjective preceding the noun, the article goes directly before the noun. Otherwise...
+        wordsInCorrectOrder.push(getRandom(articlesForThisWord), word) //
+      } else { // Splice in a random article BEFORE the last word in the array wordsInCorrectOrder, because the last word is an adjective (the only condition not satisfied by the if-statement above). After that, you can push the noun onto the end of the array. This is how you get "the green car" rather than "green the car".
+        wordsInCorrectOrder.splice(spliceAtAdjective, 0, getRandom(articlesForThisWord))
+        wordsInCorrectOrder.push(word)
+      }
+    } else { // If the word is not a noun, just push it onto the array.
+      wordsInCorrectOrder.push(word)
+    }
+  })
+
+  return wordsInCorrectOrder;
+}
